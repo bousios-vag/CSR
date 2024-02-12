298423f42dfd2d327a4fe7f9ab647d519638694f
bousios-vag <bousios.evangelos@gmail.com>
2024-02-12 17:17:04 +0200

    new sustainability report

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./dist/**/*.{html,js}",
+    "./CSR/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
